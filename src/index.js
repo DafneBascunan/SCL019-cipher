35fb04592ae9cfe554c279b4627e57088bc25e1a
DafneBascunan <dafnepaola.b@gmail.com>
2022-01-10 08:30:05 -0300

    cifrado funcional

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
+
+window.addEventListener("load",inicio,true);
+
+function inicio(){
+    document.getElementById("mensaje").addEventListener("keyup", function(){
+        this.value = this.value.toUpperCase();
+    }, true);//que todo valor de mayuscula
+    
+    document.getElementById("cifrar").addEventListener("click",function(){  
+        let texto = document.getElementById("mensaje").value;
+        let desplazamiento = document.getElementById("desplazamiento").value;               
+        document.getElementById("mensaje2").value = cipher.encode(desplazamiento,texto);
+    },true);//
+    document.getElementById("descifrar").addEventListener("click",function(){  
+        let texto = document.getElementById("mensaje").value;
+        let desplazamiento = document.getElementById("desplazamiento").value;                               
+        document.getElementById("mensaje2").value = cipher.decode(desplazamiento,texto);
+    },true);//
+}
+
+
+
+
+
 
 console.log(cipher);
